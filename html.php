169ef20f3ed042ba2defb75b23514047e86700ef
ChristopherKambayi <Christopher.Kambayi@student.ncirl.ie>
2016-12-01 16:32:16 +0000

    Add files via upload
    Chris pushing for the first time

---
+++ html.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv = "X-UA-Compatible" content = "IE=edge">
+    <meta name="viewpoint" content="width=device-width, intial-scale=1">
+
+    <title> Welcome! </title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  
+    <link href="mainpage.css" rel="stylesheet">
+
+</head>
+
+  <body>
+
+   
+      <!-- on landing page we have three buttons in center of page -->
+      <!-- colour gradient purple-->
+      <!-- button will be images
+      logout button at top
+      button 1 = Learn html!
+      button 2 = Learn css!
+      button 3 = take the quiz!
+      buttons in a jumbotrom
+      footer = web app made by blah blah-->
+      <!-- nav bar coming from http://codepen.io/davidcochran/pen/rcHae -->
+    <nav class="navbar navbar-default" role="navigation">
+        <div class="container-fluid">
+            <ul class="nav nav-pills pull-left"> 
+            <li role="presentation" class="active" onclick="return confirm('Are you sure you want to logout?')"><a href="#">Back</a></li>
+          </ul> 
+        </div>
+   </nav>
+   <!-- figured out that bootstrap has text-center in class css -->
+     
+      <div id="container">
+          <div class ="jumbotron">
+			<h1 class="text-center">Welcome!</h1>
+				<img src="img/welcome.png" alt="Random quiz logo I found online" align="middle">
+              <p> 
+				We is going to talking about HTML bitches. Better buckle up because this is going to be some mad shit.
+              </p>
+			  <p>
+			  Welcome to our quiz, I hope youre ready to learn so things are web design. This webiste will give you the help you need to improve your knowledge of some aspects of web design. Buckle up and grab a seat and sit down and prepare to be taught some shit san! This us going to be a thrill ride. THE thrill ride of your life if I may say so myself. Which I may! If you disagree then meet me outide and get ready to catch these hands fam. Anyways enjoy the quiz and if you fail then remove yourself from this page. Ciao
+			  </p>
+			  <p><strong>Hyper Text Markup Language</strong><br><br>HyperText Markup Language is the standard markup language for creating web pages and web applications.</p>
+              <a class="btn btn-default page-scroll" href="#about">Click Me to learn more</a>
+              <hr>
+          </div>
+      </div>
+      <div id="container">
+            <div class="jumbotron">
+            <h1 class="text-center">Hahaha some Information get get you started B</h1>      
+			<h3>What is HTML?</h3>
+						<li>HTML stands for Hyper Text Markup Language</li>
+						<li>HTML describes the structure of Web pages using markup</li>
+						<li>HTML elements are the building blocks of HTML pages</li>
+						<li>HTML elements are represented by tags</li>
+						<li>HTML tags label pieces of content such as "heading", "paragraph", "table", and so on</li>
+						<li>Browsers do not display the HTML tags, but use them to render the content of the page</li>
+						<br><br>
+
+					<p>HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects, such as interactive forms may be embedded into the rendered page. It provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets. Tags such as <code>&lt;img&gt; </code> &lt;and&gt; <code> &lt;span&gt; </code> introduce content into the page directly. Others such as <code>&lt;p&gt;</code> surround and provide information about document text and may include other tags as sub-elements. Browsers do not display the HTML tags, but use them to interpret the content of the page.
+
+					HTML can embed programs written in a scripting language such as JavaScript which affect the behavior and content of web pages. Inclusion of CSS defines the look and layout of content. The World Wide Web Consortium (W3C), maintainer of both the HTML and the CSS standards, has encouraged the use of CSS over explicit presentational HTML since 1997</p>
+            <hr>
+            </div>
+      </div>
+      <div id="container">
+          <div class = "jumbotron">
+              <h1 class="text-center">Little History Lesson my G</h1>
+              <p>
+				<strong>What goes here?:</strong> This is where we add information about html with external links on some headings and keywords
+              </p>
+			  <hr>
+          
+          </div>
+      </div>
+   
+      <div id="container">
+          <div class="jumbotron">
+              <h1 class="text-center">Here are some examples fam</h1>
+				<p><strong>What goes here?:</strong> Here there will be examples of html code that come 	from an XML file(s)
+				</p>
+              <hr>
+          </div>
+      </div>
+      
+      <footer>
+          <hr>
+          <p> A Web App made by : Jordan | Emma | Chris | Keith </p>
+          <hr>
+      </footer>
+      
+
+    
+ 
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/js/bootstrap.min.js"></script>
+  </body>
+</html>
